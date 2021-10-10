0eec56e06862ff9acdbfd629e6a1ab77560fb09a
Michal Takáč <michaltakac93@gmail.com>
2021-10-10 11:56:37 +0200

    pridanie bodky pre link = prefix

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./scss/style.css">
     <script src="https://kit.fontawesome.com/d41149d6de.js" crossorigin="anonymous"></script>
     <meta name="keywords" content="očná ambulancia, Livia Sedlicka , kosice">
-    <link href="/scss/style.css" rel="stylesheet">
+    
     <title> Livia Sedlická- Očný lekár</title>
 </head>
 <body>
@@ -23,7 +24,7 @@
             
             </ul> 
         </div> 
-        <img src="/logo/logo v2.svg" alt="logo" class="logo">
+        <img src="./logo/logo v2.svg" alt="logo" class="logo">
         
     </nav>
     <article id="omne">
@@ -32,7 +33,7 @@
             <div class="container">
                 <p class="card">Lorem ipsum dolor sit amet consectetur   adipisicing elit. Veniam itaque   voluptatibus at quas. Rem, qui saepe  repudiandae accusamus dolore adipisci vero   illum exercitationem veniam quod officiis     maiores. Porro, exercitationem quam quidem  dolore quaerat et pariatur at, corporis ut   est temporibus facilis nostrum repudiandae    cum. Eaque!</p>
 
-                <img src="/img/lekar.png" alt="foto lekara" class="foto">
+                <img src="./img/lekar.png" alt="foto lekara" class="foto">
 
             </div>
             <div class="otvaracie_hodiny">
@@ -147,5 +148,42 @@
         
         </div>
     </footer>
-</body>
+<!-- Code injected by live-server -->
+<script type="text/javascript">
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script></body>
 </html>
