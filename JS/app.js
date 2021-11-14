cf9c40d73468151e156890e193e91eeaea9f3c0c
Michal Takáč <michaltakac93@gmail.com>
2021-11-14 14:10:17 +0100

    upravy po rozhadzaní stránky

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,33 @@
 var sipka = document.getElementById("newclass");
     
     sipka.style.display="none";
+    
 
     window.addEventListener("resize" , function(){
         if(this.window.innerWidth >470){
     window.addEventListener("scroll", function(){
+        console.log(this.window.scrollY);
         if(this.window.scrollY >=500 ) {
         sipka.style.display="flex";
         }else{
         sipka.style.display="none";
     }
-    });
+    }); 
 
     }});
+
+    // pokusy ale nefunguje 
+    /*
+     window.addEventListener("scroll", function(){
+         var poloha= document.getElementById("position").offsetTop;
+            
+            console.log(poloha);
+         if(this.window.pageYOffset >= poloha){
+             console.log("pravda");
+         sipka.style.display="flex";
+         }else{
+              console.log("false");
+            sipka.style.display="none";
+         }
+     });   
+     */
