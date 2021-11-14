646265869f6fbcf5fa3fcfea0f00a4e3b9b65922
Michal Takáč <michaltakac93@gmail.com>
2021-11-14 12:22:40 +0100

    pridanie monilnej verzie , pridane hmaburger menu v mobile

--- JS/app.js
+++ JS/app.js
@@ -1,23 +1,15 @@
-
 var sipka = document.getElementById("newclass");
     
     sipka.style.display="none";
 
-
-window.addEventListener("scroll", function(){
-    console.log (window.scrollY)
-    if(this.window.scrollY >=1500){
-        sipka.classList.remove("backtotop");
-        sipka.classList.add("backtotop2");
-    }
-    else if(this.window.scrollY >=500 ) {
+    window.addEventListener("resize" , function(){
+        if(this.window.innerWidth >470){
+    window.addEventListener("scroll", function(){
+        if(this.window.scrollY >=500 ) {
         sipka.style.display="flex";
-        sipka.classList.remove("backtotop2");
-        sipka.classList.add("backtotop");
-
-    }else{
+        }else{
         sipka.style.display="none";
     }
-});
+    });
 
-
+    }});
