36071fddd2436dc058faf120630560131e01a005
Michal Takáč <michaltakac93@gmail.com>
2021-11-28 15:33:32 +0100

    uprava JS pre burger menu

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,13 @@
 var sipka = document.getElementById("newclass");
     
     sipka.style.display="none";
-    
+    var navigationMuv=this.document.querySelector(".navigation.moving");
+     
 
     window.addEventListener("resize" , function(){
         if(this.window.innerWidth >470){
+            
+        navigationMuv.style.display="none";
     window.addEventListener("scroll", function(){
         console.log(this.window.scrollY);
         if(this.window.scrollY >=500 ) {
@@ -14,17 +17,22 @@ var sipka = document.getElementById("newclass");
     }
     }); 
 
-    }});
+    }else{
+        burger();
+        
+    }
+    });
+ 
 
-    var burger=this.document.querySelector(".mobile");
-    var menu= document.querySelector(".menu");
-
-/* window.addEventListener("resize" , function(){
-        if(this.window.innerWidth >470){*/
-      window.addEventListener("click", function(){
+      window.addEventListener("click", function(burger){
             
+            var burger=this.document.querySelector(".mobile");
+            var menu= document.querySelector(".menu");
+
             burger.classList.toggle("active");
             menu.classList.toggle("active");
+            navigationMuv.style.display="inline-block";
+
           
-      }); 
-      
+      });
+        
