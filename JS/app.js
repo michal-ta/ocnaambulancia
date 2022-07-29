bde2742d3bd165bb42d82a929fe136bcae4e51b8
michal-ta <michaltakac93@gmail.com>
2022-07-29 07:32:38 +0200

    update of resposibility, should be update 320px

--- JS/app.js
+++ JS/app.js
@@ -1,28 +1,49 @@
-  //mapa -------------leaflet JS 
+var arrowBack = document.querySelector(".backtotop");
+var burger=document.querySelector(".mobile");
+var menu= document.querySelector(".menu");
+var navigationMuv=this.document.querySelector(".navigation.moving");
+
+ //mapa -------------leaflet JS 
 var map = L.map('map').setView([48.739580049, 21.282953024], 17);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 L.marker([48.739580049, 21.282953024]).addTo(map);
-L.marker.bindPopup("Sed Med s.r.o").openPopup();
 
 //----------------------------------------
 
 
-//-- adding of back arrow , changing vissibilty of burger menu -------
-var arrowBack = document.getElementById("newclass");
-var burger=document.querySelector(".mobile");
-var menu= document.querySelector(".menu");
+//-- showing  of back arrow after scroll down  , 
+(function changearow(){
+    let scroling =document.addEventListener("scroll",function(){
+        var scrollPosition =window.scrollY;
+        
+    });
+
+    let resizing=window.addEventListener("resize" ,function(){
+        var actualWidth=window.innerWidth;
+        
+    });
+
+}());
+/*if (scrollPosition <=850){
+    arrowBack.style.display="none"
+}else{
+    arrowBack.style.display="flex"
+}*/
+
+// change to dont show arrow in mobile version 
+
+ /*   if(this.innerWidth <=570){
+        arrowBack.style.display="none"
+    }else{
+        arrowBack.style.display="flex"
+    }*/
 
 
     
-    arrowBack.style.display="none";
-    var navigationMuv=this.document.querySelector(".navigation.moving");
-
-
-    
- //--------------------------------------   
+//--------------------------------------   
 /*
     window.addEventListener("resize" , function(burger1){
         if(this.window.innerWidth <470){
